739304e66081161e05fa399b4a3afa5703a66980
Rahul A B <rahulbushi69@gmail.com>
2022-11-23 19:20:12 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ import { useState } from 'react';
 
 export default function Home() 
 {
-  const[darkMode,setDarkMode] = useState(false);
+  const[darkMode,setDarkMode] = useState(true);
   return (
     <div className={darkMode ? "dark":""}>
       <Head>
@@ -29,10 +29,10 @@ export default function Home()
       <main className='" bg-white px-10 m:px-20 lg:px-40 dark:bg-gray-900'>
         <section className="min-h-screen" >
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-vt323 lg:text-3xl dark:text-white'>Developed By Rahul</h1>
+            <h1 className='text-xl font-vt323 lg:text-3xl text-black dark:text-white'>Developed By Rahul</h1>
             <ul className='flex items-center'>
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=' cursor-pointer text-l  lg:text-xl dark:text-white'/>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='text-black cursor-pointer text-l  lg:text-xl dark:text-white'/>
               </li>
               <li>
                 <a href="#" className=' bg-gradient-to-r from-cyan-400 dark:from-cyan-800 to-teal-800 dark:to-teal-400 text-white px-2 py-1 rounded-md ml-8 lg:px-4 lg:py-2'>Resume</a>
@@ -40,10 +40,10 @@ export default function Home()
             </ul>
           </nav>
           <div className=' text-center p-10 '>
-            <h2 className=' text-5xl py-2 text-teal-800 dark:text-teal-400 font-medium md:text-6xl '>
+            <h2 className='font-burtons text-5xl py-2 text-teal-800 dark:text-teal-400 font-medium md:text-6xl '>
               Rahul A B
             </h2>
-            <h3 className=' text-2xl py-2 md:text-3xl dark:text-white'>
+            <h3 className=' text-2xl py-2 md:text-3xl text-black dark:text-white'>
               Developer and Designer
             </h3>
             <p className=' text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white' >
@@ -80,12 +80,12 @@ export default function Home()
               </a>
             </div>
           </div>
-          <div className='relative mx-auto bg-gradient-to-b from-teal-800 dark:from-teal-400 to-slate-200 rounded-full w-80 h-80 mt-20 overflow-hidden lg:h-100 lg:w-100'>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-800 dark:from-teal-400 to-slate-200 rounded-full w-60 h-60 mt-10 overflow-hidden lg:h-100 lg:w-100'>
             <Image src={deved} layout="fill" objectFit="cover"/> 
           </div>
         </section>
 
-        <section>
+        <section className='mt-5'>
           <div>
             <h3 className='text-2xl py-1 lg:text-3xl dark:text-teal-400 text-teal-800'>Services That I Offer</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
@@ -96,18 +96,18 @@ export default function Home()
           <div className='lg:flex gap-10'>
             <div className="text-center mx-auto shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 dark:text-white flex-1">
               <Image className='mx-auto' src={design} width={100} height={100} />
-              <h3 className='text-lg font-medium pt-8 pb-2 dark:text-teal-400'>Beautiful Designs</h3>
-              <p className='py-2'>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-800 dark:text-teal-400'>Beautiful Designs</h3>
+              <p className='py-2 text-blac dark:text-white'>
                 Creative elegant designs suited for your needs following core design theory.
               </p>
-              <h3 className='text-teal-600 dark:text-teal-400 py-4'>Design Tools I Use:</h3>
+              <h3 className='text-teal-600 t dark:text-teal-400 py-4'>Design Tools I Use:</h3>
               <p className='text-gray-800 py-1 dark:text-white'>Photoshop</p>
               <p className='text-gray-800 py-1 dark:text-white'>Illustrator</p>
             </div>
             <div className="text-center mx-auto shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1 dark:text-white">
               <Image className='mx-auto' src={code} width={100} height={100} />
-              <h3 className='text-lg font-medium pt-8 pb-2 dark:text-teal-400'>Coding Language</h3>
-              <p className='py-2'>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-800 dark:text-teal-400'>Coding Language</h3>
+              <p className='py-2 text-blac dark:text-white'>
                 The languages that I am knowlegable in is as follows.
               </p>
               <h3 className='text-teal-600 dark:text-teal-400 py-4'>Languages I Use Are:</h3>
@@ -116,10 +116,10 @@ export default function Home()
               <p className='text-gray-800 py-1 dark:text-white'>Java</p>
               <p className='text-gray-800 py-1 dark:text-white'>Python</p>
             </div>
-            <div className="text-center mx-auto shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700 flex-1 dark:text-white">
+            <div className="text-center mx-auto shadow-lg p-10 rounded-xl my-10 text-teal-800 dark:bg-gray-700 flex-1 dark:text-white">
               <Image className='mx-auto' src={databases} width={100} height={100} />
               <h3 className='text-lg font-medium pt-8 pb-2 dark:text-teal-400'>Databases </h3>
-              <p className='py-2'>
+              <p className='py-2 text-blac dark:text-white'>
                 The Databases that I am knowlegable in is as follows.
               </p>
               <h3 className='text-teal-600 dark:text-teal-400 py-4'>Databases I Use Are:</h3>
@@ -127,9 +127,6 @@ export default function Home()
             </div>
           </div>
         </section>
-
-
-        
       </main>
     </div>
   );
